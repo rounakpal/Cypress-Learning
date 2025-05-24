96294e315cf941c8245e824f92de02234207bd95
rounakpal <rounakpal902@gmail.com>
2025-05-24 19:16:35 +0530

    Commit all pending changes and single buil

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      pageLoadTimeout: 120000
+      pageLoadTimeout: 120000,
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
